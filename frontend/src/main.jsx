8b9bb98b62c5caab70e0da3ab1b728bf24197f00
sachin-jandy-rhoods <sachinjandyrhoods@gmail.com>
2024-10-06 19:33:27 +0530

    add AllMovie in page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import store from "./redux/store.js";
 import { Provider } from "react-redux";
 import { Route, RouterProvider, createRoutesFromElements } from "react-router";
 import { createBrowserRouter } from "react-router-dom";
-import Home from "./pages/Home.jsx";
+
 
 //Auth
 
@@ -14,10 +14,14 @@ import Home from "./pages/Home.jsx";
 import Login from "./pages/Auth/Login.jsx";
 import Register from "./pages/Auth/Register.jsx";
 
+import Home from "./pages/Home.jsx";
+import AllMovies from "./pages/Movies/AllMovies.jsx"
+
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<Home />} />
+      <Route path="/movies" element={<AllMovies />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
 
